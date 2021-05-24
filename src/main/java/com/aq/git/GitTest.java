33618c0bde5687de2b4f88f88a96fc7de9dfc849
AQ <AQ@bigdata.com>
2021-05-24 20:00:38 +0800

    first commit

---
+++ src/main/java/com/aq/git/GitTest.java
@@ -0,0 +1,18 @@
+package com.aq.git;
+
+/**
+ * Author: AQ
+ * Date: 2021/5/24 19:54
+ * Desc:
+ */
+public class GitTest {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("hello git");
+
+    }
+
+}
