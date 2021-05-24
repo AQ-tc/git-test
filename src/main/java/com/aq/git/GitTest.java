aef54535563eb82a179f92a01d78cf8d12d09e7c
AQ <AQ@bigdata.com>
2021-05-24 20:51:15 +0800

    hot-fix commit1

--- src/main/java/com/aq/git/GitTest.java
+++ src/main/java/com/aq/git/GitTest.java
@@ -7,14 +7,11 @@ package com.aq.git;
  */
 public class GitTest {
 
-
     public static void main(String[] args) {
-
 
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
-
+        System.out.println("hello git4");
     }
-
 }
